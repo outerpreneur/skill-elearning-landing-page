ad50df313ac35dc0cedca48a45f3f041eb282eb0
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2024-06-23 23:58:37 +0200

    initial estructure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import Grid from "./components/Grid";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <Header />
+      <main>
+        <Hero />
+        <Grid />
+      </main>
+      <Footer />
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div> */}
+    </body>
   );
 }
 
---
+++ src/components/Grid.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Grid() {
+  return (
+    <header>
+      <h1>Grid</h1>
+    </header>
+  );
+}
